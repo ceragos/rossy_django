c4b4a1678341be38c2c2a291a87eebb40924afbb
ceragos <jesus.a.castaneda.v@gmail.com>
2019-10-06 02:34:40 -0500

    Administrador Modulo bodega

--- aplicacion/bodega/admin.py
+++ aplicacion/bodega/admin.py
@@ -1,9 +1,70 @@
 from django.contrib import admin
 
-# Register your models here.
-from aplicacion.bodega.models import Insumo, InventarioInsumo, Producto, InventarioProducto
+from aplicacion.bodega.models import Insumo, InventarioInsumo, Producto, InventarioProducto, UnidadMedida
+from aplicacion.utilidades.admin import AdminModel
 
-admin.site.register(Insumo)
-admin.site.register(InventarioInsumo)
-admin.site.register(Producto)
-admin.site.register(InventarioProducto)
+
+@admin.register(UnidadMedida)
+class UnidadMedidaAdmin(AdminModel):
+    fieldsets = (
+        ('Unidad de Medida', {
+            'fields': ('nombre', 'abreviatura')
+        }),
+        ('Auditoria', {
+            'fields': ('activo', 'creado_por', 'fecha_creacion', 'ip_creacion',
+                       'modificado_por', 'fecha_modificacion', 'ip_modificacion')
+        }),
+    )
+
+
+@admin.register(Insumo)
+class InsumoAdmin(AdminModel):
+    fieldsets = (
+        ('Información del Insumo', {
+            'fields': ('nombre', 'cantidad', 'unidad_medida')
+        }),
+        ('Auditoria', {
+            'fields': ('activo', 'creado_por', 'fecha_creacion', 'ip_creacion',
+                       'modificado_por', 'fecha_modificacion', 'ip_modificacion')
+        }),
+    )
+
+
+@admin.register(InventarioInsumo)
+class InventarioInsumoAdmin(AdminModel):
+    fieldsets = (
+        ('Inventario de Insumos', {
+            'fields': ('insumo', 'unidades', 'precio_unitario')
+        }),
+        ('Auditoria', {
+            'fields': ('activo', 'creado_por', 'fecha_creacion', 'ip_creacion',
+                       'modificado_por', 'fecha_modificacion', 'ip_modificacion')
+        }),
+    )
+
+
+@admin.register(Producto)
+class ProductoAdmin(AdminModel):
+    fieldsets = (
+        ('Información del Producto', {
+            'fields': ('nombre', 'descripcion')
+        }),
+        ('Auditoria', {
+            'fields': ('activo', 'creado_por', 'fecha_creacion', 'ip_creacion',
+                       'modificado_por', 'fecha_modificacion', 'ip_modificacion')
+        }),
+    )
+
+
+@admin.register(InventarioProducto)
+class InventarioProductoAdmin(AdminModel):
+    fieldsets = (
+        ('Inventario de Productos', {
+            'fields': ('producto', 'unidades', 'unidad_medida', 'precio_costo', 'precio_venta', 'fecha_elaboracion',
+                       'fecha_vencimiento')
+        }),
+        ('Auditoria', {
+            'fields': ('activo', 'creado_por', 'fecha_creacion', 'ip_creacion',
+                       'modificado_por', 'fecha_modificacion', 'ip_modificacion')
+        }),
+    )
