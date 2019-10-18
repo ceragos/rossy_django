e101c5af1f55a6ffbaad873afe22aabcfde6264c
ceragos <jesus.a.castaneda.v@gmail.com>
2019-10-18 10:29:01 -0500

    Implementación boostrap

--- aplicacion/utilidades/urls.py
+++ aplicacion/utilidades/urls.py
@@ -1,7 +1,8 @@
+from django.contrib.auth.decorators import login_required
 from django.urls import path
 
 from aplicacion.utilidades.views import IndexTemplateView
 
 urlpatterns = [
-    path('', IndexTemplateView.as_view(), name = 'index'),
+    path('', (IndexTemplateView.as_view()), name = 'index'),
 ]
