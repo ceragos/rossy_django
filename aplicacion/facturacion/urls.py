8b1e38f34df9742afb7b6cab9df6b297cce76708
ceragos <jesus.a.castaneda.v@gmail.com>
2020-03-12 14:16:28 -0500

    url para el uso de la vista de creación de documento pdf

--- aplicacion/facturacion/urls.py
+++ aplicacion/facturacion/urls.py
@@ -1,8 +1,9 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
 
-from aplicacion.facturacion.views import CreditosCobrarListView
+from aplicacion.facturacion.views import CreditosCobrarListView, FacturaPDF
 
 urlpatterns = [
     path('credito_cobrar/', login_required(CreditosCobrarListView.as_view()), name = 'facturacion.credito_cobrar'),
+    path(r'factura_pdf/(?P<factura_id>[-_:\w\d]+)', FacturaPDF.as_view(), name='factura_pdf'),
 ]
